5b860bf0796749baf154ebc9f5d2850e3e5fc99f
NathanBaldwin <you@example.com>
2015-11-12 16:07:09 -0600

    stashing changes

--- javascripts/promises.js
+++ javascripts/promises.js
@@ -4,7 +4,8 @@ requirejs.config({
     'jquery': '../lib/bower_components/jquery/dist/jquery.min',
     'lodash': '../lib/bower_components/lodash/lodash.min',
     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    "q": "../lib/bower_components/q/q"
   },
   shim: {
     'bootstrap': ['jquery']
